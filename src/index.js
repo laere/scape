0cc36988f445cb198ce08eae341bfcab42199448
Zack Bostian <bostianz@gmail.com>
2016-05-01 14:21:19 -0400

    added dependancies and fleshed out folder structure

--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { App } from './App';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Perf from 'react-addons-perf';
+//  STORE METHODS
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
+// MIDDLEWARE
+import Thunk from 'redux-thunk';
+import Promise from 'redux-promise';
+// ROUTING METHODS
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+// CONTAINERS
+import App from './app/App';
+//REDUCERS
+import rootReducer from './reducers/RootReducer';
+// Store with middleware.
+const createStoreWithMiddleware = compose(
+  applyMiddleware(Thunk, Promise),
+  window.devToolsExtension ? window.devToolsExtension() : (f) => f
+)(createStore);
 
-render(<App />, document.getElementById('root'));
+const store = createStoreWithMiddleware(reducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      <Route path='/' component={App} >
+      </Route>
+    </Router>
+  </Provider>,
+  document.querySelector('.container')
+);
