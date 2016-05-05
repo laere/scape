d946084687331a7c4bc01f2b2ac60b122cc8c1fd
Zack Bostian <bostianz@gmail.com>
2016-05-05 14:05:08 -0400

    added about page info

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,30 @@ import React, { Component, PropTypes } from 'react';
 export default class About extends Component {
   render() {
     return (
-      <div></div>
+      <div className="aboutPage">
+        <h1>Welcome to Scape!</h1>
+        <p>Scape is a web application that allows users to look up monsters, personal hiscores, grand exchange information and more!</p>
+
+        <p>I created this site, because I absolutely have enjoyed 15 years (with some breaks) of Runescape</p>
+
+        <p>
+          I started playing Runescape in 2001, and immediately enjoyed it's skilling system and combat. It also was a very social game as the
+          GE didn't exist back then, so you had to go to popular worlds, and cities within to find players selling what you were buying and vice versa. This also established
+          economical partnerships between people and friendships in general.
+
+          Runescape reached it's peak in 2007-2009, in my opinion. Runescape 2 was amazing, and having mass wilderness PK battles between clans was absolutely awesome. Nothing like
+          having 100+ people in the wildy just going at it. And more. Overall it was just a great time for Runescape, and I had a lot of friends to play with at the time.
+
+          I think today Runescape has evolved greatly, it's still a great game, but like every game it has it's faults. EOC I think is sort of killed the combat that made runescape so fun.
+          MTX is a typical cash grab, and although I loved that Jagex brought back OSRS servers, but it split the community in half, and Runescape has always had an amazing community.
+
+          All in all I still enjoy Runescape, and it's still my first MMO ever, so I do have a bit of bias.
+        </p>
+
+        <p>I hope you guys enjoy this site, as I would like to make it into something big!</p>
+
+        <p>- Zack</p>
+      </div>
     );
   }
 }
