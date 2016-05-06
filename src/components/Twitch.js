31a0ec38b6bcc745d2165b5489dec15efb152fe1
Zack Bostian <bostianz@gmail.com>
2016-05-06 13:24:53 -0400

    fixed loading data flag for when streams are being fetched

--- src/components/Twitch.js
+++ src/components/Twitch.js
@@ -1,9 +1,26 @@
 import React, { Component, PropTypes } from 'react';
 
-export default class Twitch extends Component {
-  render() {
+const Twitch = ({ streams }) => {
     return (
-      <div>Placeholder</div>
-    );
-  }
+      <div className="streamsList">
+        {streams.map(stream => {
+          return (
+            <div key={stream._id} className="streamsList__item">
+              <span>{stream.channel.game}</span>
+              <div>
+                <a href={stream.channel.url} target="_blank">
+                  <img src={stream.preview.medium}/>
+                </a>
+              </div>
+              <div>
+                <span>{stream.viewers} viewers on <strong>{stream.channel.display_name}</strong></span>
+              </div>
+            </div>
+          );
+        }
+      )}
+    </div>
+  );
 }
+
+export default Twitch;
--- src/containers/TwitchContainer.js
+++ src/containers/TwitchContainer.js
@@ -1,29 +1,32 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { fetchData } from '../actions/DataFetching';
-import RS_URL from '../endpoints/endpoints';
 import Twitch from '../components/Twitch';
 
 class TwitchContainer extends React.Component {
   static propTypes = {
-
+    twitchData: PropTypes.object,
+    fetchStreams: PropTypes.func.isRequired
   };
 
   componentDidMount() {
-    const { fetchStreams } = this.props;
-    fetchStreams('https://api.twitch.tv/kraken/streams?game=runescape');
+    const { fetchStreams, twitchData } = this.props;
+    if (!twitchData.data) {
+      fetchStreams('https://api.twitch.tv/kraken/streams?game=runescape&limit=100');
+    }
   }
 
   render() {
-    return (
-      <Twitch />
-    );
+    const { twitchData } = this.props;
+    return twitchData.isFetching ?
+      <h1 style={{textAlign: 'center', fontSize: '6rem'}}>Loading data...</h1> :
+      <Twitch streams={twitchData.data.streams}/>;
   }
 }
 
 const mapStateToProps = (state) => {
   return {
-    streams: state.twitchData.data
+    twitchData: state.twitchData
   }
 }
 
