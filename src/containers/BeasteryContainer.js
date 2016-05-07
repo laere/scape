25fc1f845a28dbf7bfb290fac38b158ef4464708
Zack Bostian <bostianz@gmail.com>
2016-05-07 13:12:11 -0400

    added basic functions and import actions to beastery container

--- src/containers/BeasteryContainer.js
+++ src/containers/BeasteryContainer.js
@@ -6,32 +6,42 @@ import Beastery from '../components/Beastery';
 
 class BeasteryContainer extends React.Component {
   static propTypes = {
-
+    text: PropTypes.string.isRequired,
+    getInput: PropTypes.func.isRequired,
+    clearInput: PropTypes.func.isRequired,
+    getBeasteryData: PropTypes.func.isRequired
   };
 
   constructor() {
     super();
-    // this.handleOnSubmit = this.handleOnSubmit.bind(this);
-    // this.handleOnChange = this.handleOnChange.bind(this);
+    this.handleOnSubmit = this.handleOnSubmit.bind(this);
+    this.handleOnChange = this.handleOnChange.bind(this);
+    this.handleOnClick = this.handleOnClick.bind(this);
   }
 
   handleOnSubmit(e) {
     e.preventDefault();
+    const { getBeasteryData, text } = this.props;
+    //getBeasteryData(url + text);
   }
 
   handleOnClick(e) {
     e.preventDefault();
+    this.props.clearInput();
   }
 
   handleOnChange(e) {
     e.preventDefault();
-
+    this.props.getInput(text);
   }
 
   render() {
-    console.log(this.props.text);
     return (
-      <Beastery />
+      <Beastery
+        text={text}
+        onSubmit={this.handleOnSubmit}
+        onClick={this.handleOnSubmit}
+        onChange={this.handleOnChange} />
     );
   }
 }
